04a8871cc586d7e4667679f80d4df023634d224e
Aykut Yılmaz <aykut.yilmaz@akinon.com>
2021-04-12 21:03:29 +0300

    Update bootstrap import

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 <script>
 import Layout from "./layout/Index";
 import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 export default {
   name: "App",
   components: {
